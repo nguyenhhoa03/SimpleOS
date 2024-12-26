748025d01c1ce891356cb4b9dd47de4e2b4d7c1d
nguyenhhoa03 <nguyenhhoa03@gmail.com>
2024-12-26 11:21:25 +0700

    Update apps setup.sh

--- setup.sh
+++ setup.sh
@@ -17,4 +17,32 @@ apt install gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 reboot
 
+# Productivity apps
+flatpak install flathub md.obsidian.Obsidian
+flatpak install flathub org.onlyoffice.desktopeditors
+
+# Design apps
+flatpak install flathub org.gimp.GIMP
+flatpak install flathub org.inkscape.Inkscape
+flatpak install flathub page.kramo.Sly
+
+# Media apps
+flatpak install flathub org.videolan.VLC
+flatpak install flathub org.kde.kdenlive
+flatpak install flathub org.audacityteam.Audacity
+flatpak install flathub org.gnome.NetworkDisplays
+
+# Development apps
+flatpak install flathub com.visualstudio.code
+flatpak install flathub com.getpostman.Postman
+flatpak install flathub cc.arduino.IDE2
+flatpak install flathub re.sonny.Workbench
+
+# Gaming apps
+flatpak install flathub net.lutris.Lutris
+flatpak install flathub ru.linux_gaming.PortProton
+
+# Education apps
+flatpak install flathub org.stellarium.Stellarium
+flatpak install flathub org.geogebra.GeoGebra
 
